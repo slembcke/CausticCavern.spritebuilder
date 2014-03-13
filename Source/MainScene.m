24e899c150d6149ee9810cdf372fbaa020b712d6
slembcke <slembcke@gmail.com>
2014-03-13 16:37:46 -0500

    Using the new clear command

--- Source/MainScene.m
+++ Source/MainScene.m
@@ -199,52 +199,46 @@ LightVertex(GLKMatrix4 transform, GLKVector2 pos, GLKVector2 texCoord, GLKVector
 
 -(void)visit:(CCRenderer *)renderer parentTransform:(const GLKMatrix4 *)parentTransform
 {
-/*
-Possible fillrate reduction methods if needed:
-* Half-res render buffer.
-* Backface culling.
-* Scissoring.
-*/
-	
 	CGAffineTransform worldToLight = self.worldToNodeTransform;
 	GLKMatrix4 projection = _lightMapBuffer.projection;
 	
 	float ambient = 0.3;
 	[_lightMapBuffer beginWithClear:ambient g:ambient b:ambient a:0];
-	
-	for(CCNode<Light> *light in _lights){
-		[renderer enqueueBlock:^{
-			glEnable(GL_CULL_FACE);
-			glCullFace(GL_BACK);
+		for(CCNode<Light> *light in _lights){
+			// TODO enabling scissoring could reduce fillrate usage here.
 			
-			glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_TRUE);
+			[renderer enqueueBlock:^{
+				// Disable drawing the back faces to cut down on fillrate.
+				glEnable(GL_CULL_FACE);
+				glCullFace(GL_BACK);
+				
+				// The shadow mask should only affect the alpha chanel.
+				glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_TRUE);
+			} debugLabel:@"LightingLayer: Set shadow mask drawing mode."];
 			
-#warning TODO renderer should have a clear command.
-			glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-			glClear(GL_COLOR_BUFFER_BIT);
-		} debugLabel:@"LightingLayer: Set shadow mask drawing mode."];
-		
-		[self maskLight:light renderer:renderer worldToLight:worldToLight projection:projection];
-		
-		[renderer enqueueBlock:^{
-			glDisable(GL_CULL_FACE);
+			// Clear the alpha and draw the shadow mask.
+			[renderer enqueueClear:GL_COLOR_BUFFER_BIT color:GLKVector4Make(0.0f, 0.0f, 0.0f, 0.0f) depth:0.0f stencil:0];
+			[self maskLight:light renderer:renderer worldToLight:worldToLight projection:projection];
 			
-			glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
-		} debugLabel:@"LightingLayer: Restore mode."];
-		
-		CGPoint pos = light.position;
-		float radius = light.lightRadius;
-		GLKVector4 color4 = light.lightColor;
-		
-		CCRenderBuffer buffer = [renderer enqueueTriangles:2 andVertexes:4 withState:_lightRenderState];
-		CCRenderBufferSetVertex(buffer, 0, LightVertex(projection, GLKVector2Make(pos.x - radius, pos.y - radius), GLKVector2Make(-1, -1), color4));
-		CCRenderBufferSetVertex(buffer, 1, LightVertex(projection, GLKVector2Make(pos.x - radius, pos.y + radius), GLKVector2Make(-1,  1), color4));
-		CCRenderBufferSetVertex(buffer, 2, LightVertex(projection, GLKVector2Make(pos.x + radius, pos.y + radius), GLKVector2Make( 1,  1), color4));
-		CCRenderBufferSetVertex(buffer, 3, LightVertex(projection, GLKVector2Make(pos.x + radius, pos.y - radius), GLKVector2Make( 1, -1), color4));
-		CCRenderBufferSetTriangle(buffer, 0, 0, 1, 2);
-		CCRenderBufferSetTriangle(buffer, 1, 0, 2, 3);
-	}
-	
+			// Reset culling and color masking.
+			[renderer enqueueBlock:^{
+				glDisable(GL_CULL_FACE);
+				glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+			} debugLabel:@"LightingLayer: Restore mode."];
+			
+			// Render a quad for the light.
+			CGPoint pos = light.position;
+			float radius = light.lightRadius;
+			GLKVector4 color4 = light.lightColor;
+			
+			CCRenderBuffer buffer = [renderer enqueueTriangles:2 andVertexes:4 withState:_lightRenderState];
+			CCRenderBufferSetVertex(buffer, 0, LightVertex(projection, GLKVector2Make(pos.x - radius, pos.y - radius), GLKVector2Make(-1, -1), color4));
+			CCRenderBufferSetVertex(buffer, 1, LightVertex(projection, GLKVector2Make(pos.x - radius, pos.y + radius), GLKVector2Make(-1,  1), color4));
+			CCRenderBufferSetVertex(buffer, 2, LightVertex(projection, GLKVector2Make(pos.x + radius, pos.y + radius), GLKVector2Make( 1,  1), color4));
+			CCRenderBufferSetVertex(buffer, 3, LightVertex(projection, GLKVector2Make(pos.x + radius, pos.y - radius), GLKVector2Make( 1, -1), color4));
+			CCRenderBufferSetTriangle(buffer, 0, 0, 1, 2);
+			CCRenderBufferSetTriangle(buffer, 1, 0, 2, 3);
+		}
 	[_lightMapBuffer end];
 	
 	[super visit:renderer parentTransform:parentTransform];
